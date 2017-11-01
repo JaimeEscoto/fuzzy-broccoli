0d936beae8447f185d41cfc3ff0989f4ffec5aa1
Jaime Escoto <jaime.escoto@hotmail.com>
2017-11-01 17:36:03 -0600

    yes
    yes

---
+++ Exercises/hola mundo.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Ejercicio Hola Mundo </title>
+  </head>
+  <body>
+    <h1>Ejercicio Hola Mundo </h1>
+    <h2>Mi primer ejercicio en PHP</h2>
+    <?php
+      echo "Hola Mundo";
+      echo "Hi World";
+      echo "test";
+      
+    ?>
+  </body>
+</html>
---
+++ Exercises/notas.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Ejercicio Notas</title>
+  </head>
+  <body>
+    <h1>Ejercicio Notas</h1>
+    <h2>Usando variables y la sentencia if en PHP</h2>
+    <?php
+      $notaAcumulado=40;
+      $notaExamen=30;
+      $notaFinal=$notaAcumulado+$notaExamen;
+    ?>
+    <table border="2">
+      <tr>
+        <td>Nota Acumulado</td>
+        <td>Nota Examen</td>
+        <td>Nota Final</td>
+        <td>Observación</td>
+      </tr>
+      <tr>
+        <td><?php echo $notaAcumulado ?></td>
+        <td><?php echo $notaExamen ?></td>
+        <td><?php echo $notaFinal ?></td>
+        <td>
+          <?php
+              /*
+              Si la $notaFinal es >= que 60  -> imprime Aprobado
+              Si la $notaFinal es < 60       -> imprime Reprobado
+              */
+              if ($notaFinal>=60) {
+                echo "Aprobado";
+              }
+              else {
+                echo "Reprobado";
+              }
+          ?>
+        </td>
+      </tr>
+    </table>
+  </body>
+</html>
