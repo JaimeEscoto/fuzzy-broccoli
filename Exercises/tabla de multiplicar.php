df1969f9b3fffc39ed0939eaf124103b77a74a3c
Jaime Escoto <jaime.escoto@hotmail.com>
2017-11-01 21:17:53 -0600

    Publicando Tabla.
    Subiendo el archivo php de ejercicio de tabla de multiplicar, pendiente el formulario.

---
+++ Exercises/tabla de multiplicar.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Ejercicio Tabla de Multiplicar</title>
+  </head>
+  <body>
+    <h1>Ejercicio Tabla de Multiplicar</h1>
+    <h2>Usando formulario con metodo post</h2>
+
+    <?php $numero=3; ?>
+    <h3>Tabla del <?php echo $numero; ?></h3>
+    
+  </body>
+</html>
