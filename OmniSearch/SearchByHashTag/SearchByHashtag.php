a51dff64ddd395780370e1b1c8fdc6026e275297
Jaime Escoto <jaime.escoto@hotmail.com>
2017-11-07 02:04:54 -0600

    Update to Intagram Search HashTag

---
+++ OmniSearch/SearchByHashTag/SearchByHashtag.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+
+
+<?php
+  //Aca configuramos para que puedan obtener las publicaciones
+
+    $hashTag = $_POST["hashTag"];
+    $photos = $_POST["photos"];
+    $url = "https://www.instagram.com/explore/tags/$hashTag/?__a=1";
+
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 20);
+
+
+    $result = curl_exec($ch);
+    curl_close($ch);
+
+    $result = json_decode($result);
+//
+//print_r($result->items->id);
+//echo ($result->items->id);
+
+//Esta variable servira como contador, asi podre limitar la cantidad de post que va a mostrar
+$i = 0;
+?>
+<table border="2" align="center" >
+
+<tr>
+
+<td>
+
+<h1><?php echo $result->tag->name ?></h1>
+</td>
+</tr>
+</table>
+
+ <?php     foreach($result->tag->media->nodes as $post): ?>
+  <a href="<?php echo $post->display_src ?>" target="blank" title="<?php echo $post->caption ?>">
+      <img src="<?php echo $post->thumbnail_src ?>" alt="<?php echo $post->caption ?>" height="150" width="150" />
+  </a>
+
+<?php
+//Si la cantidad de post alcanzo 8, ya no mostrar
+if (++$i == $photos)
+
+
+//Fin
+break; endforeach ?>
+
+
+
+  </body>
+</html>
